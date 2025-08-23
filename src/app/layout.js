f7fb432a766b5fb9c507927e3a2409ba7b719814
Santiago Gómez de la Torre Romero <81492002+sgomez-dev@users.noreply.github.com>
2025-08-23 22:59:15 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "../styles/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "@/styles/globals.css";
 
 export const metadata = {
   title: "Santiago Gómez de la Torre Romero Portfolio",
